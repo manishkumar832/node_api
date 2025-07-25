1b49ef23cb9372006bcb8dfc3c8ba7b812166ede
manishkumar832 <bodakuntimanishkumar@gmail.com>
2025-07-25 16:54:38 +0530

    updated file

--- config/DbConfig.js
+++ config/DbConfig.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose")
-const url="mongodb://localhost:27017/"
+const url="mongodb+srv://bodakuntimanishkumar:QVPlZYGOsbzPpDwb@cluster0.w7fbioz.mongodb.net/"
+// const uri="mongodb://localhost:27017/"
 
 async function mongodb(){
    try {
